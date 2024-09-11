141e21b5941fc68cc8bf6f66f7a3ecb8404066af
하루이 <125961256+develsvai@users.noreply.github.com>
2024-09-11 11:35:04 +0900

    [Bronze III] Title: 최댓값, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,9 +1,29 @@
+def find_index_max(arr) : 
+    
+    tmp = arr[0]
+    tmp_index =0 
+    
+    for i in range(len(arr)) :
+        if arr[i] > tmp :
+            tmp = arr[i]
+            tmp_index = i
+    return tmp , tmp_index+1
 
-lis = []
 
-for i in range(0,9) :
-    lis.append(int(input()))
+num = []
+
+for i in range(9) : 
+    
+
+    user_input = input()
+
+    if user_input == "" :
+        break
+
+    num.append(int(user_input))
 
 
-print(max(lis))
-print(lis.index(max(lis))+1) 
+max_val, max_index = find_index_max(num)
+
+print(max_val)
+print(max_index)
